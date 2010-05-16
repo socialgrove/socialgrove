87a15c8b54589ec70eefa46c1696cfdf81e70212
ashok <ashok@chromosomeconsulting.com>
2010-05-16 14:10:08 -0500

    additional authentication details

--- src/main/java/authentication/CallbackServlet.java
+++ src/main/java/authentication/CallbackServlet.java
@@ -2,12 +2,16 @@ package authentication;
 
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
+import twitter4j.User;
 import twitter4j.http.RequestToken;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import db.UserDB;
+
 import java.io.IOException;
 
 public class CallbackServlet extends HttpServlet {
@@ -23,6 +27,25 @@ public class CallbackServlet extends HttpServlet {
         } catch (TwitterException e) {
             throw new ServletException(e);
         }
+        User user;
+        UserDB userDB = new UserDB();
+		try {
+			user = twitter.showUser(twitter.getId());
+			System.out.println("user.getId(): "+user.getId());
+			System.out.println("twitter.getId(): "+twitter.getId());
+			if(!userDB.checkTwitterUser(user.getId())){
+				System.out.println("Storing Twitter User Details");
+				userDB.insertTwitterUser(user.getId(), user.getName(), user.getProfileImageURL().toString(), user.getScreenName(), user.getLocation(), user.getURL().toString());
+			}
+			
+			System.out.println("Screen Name: "+user.getScreenName());
+			System.out.println("Image URL: "+user.getProfileImageURL());
+			
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
         response.sendRedirect(request.getContextPath()+ "/main.html");
     }
 }
--- src/main/java/db/UserDB.java
+++ src/main/java/db/UserDB.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 import common.User;
 import common.UserList;
@@ -37,5 +38,47 @@ public class UserDB {
             e.printStackTrace();
         }
         return list;
-	}	
+	}
+	
+	public boolean checkTwitterUser(int twitterId){
+		try {
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(url,"postgres","beer123");
+            String sql = "select * from socialgrove.\"twitterUser\" where \"twitterId\"= ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, twitterId);
+            ResultSet rs;
+            rs = stmt.executeQuery();
+            while ( rs.next() ) {
+            	return true;
+            }
+		} catch (Exception e) {
+            System.err.println("Got an exception! ");
+            e.printStackTrace();
+        }
+		return false;
+	}
+	
+	public boolean insertTwitterUser(int twitterId, String twitterName, String twitterImageURL, String twitterScreenName, String twitterLocation, String twitterURL){
+		try {
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(url,"postgres","beer123");
+            String sql = "insert into socialgrove.\"twitterUser\" values(?,?,?,?,?,?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, twitterId);
+            stmt.setString(2, twitterName);
+            stmt.setString(3, twitterImageURL);
+            stmt.setString(4, twitterScreenName);
+            stmt.setString(5, twitterLocation);
+            stmt.setString(6, twitterURL);
+            stmt.executeUpdate();
+            return true;
+		} catch (Exception e) {
+            System.err.println("Got an exception! ");
+            e.printStackTrace();
+        }
+		return false;
+	}
 }
